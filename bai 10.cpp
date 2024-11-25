98026f7697b4503fa536dcc50114288b6c0682fd
nguyentienloc653 <locnguyentien653@gmail.com>
2024-11-25 13:34:40 +0700

    upload ss4-10

---
+++ bai 10.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Nhap vao ba chu so:");
+    scanf("%d %d %d", &a , &b, &c);
+    
+    if(a>b&&a>c&&b>c){
+    	printf("%d %d %d",c,b,a);
+	}
+	
+	else if(a>c&&a>b&&c>b){
+		printf("%d %d %d",b,c,a);
+	}
+	else if(b>a&&b>c&&a>c){
+		printf("%d %d %d",c,a,b);
+	}
+	else if(b>a&&b>c&&c>a){
+		printf("%d %d %d",a,c,b);
+	}
+	else if(c>a&&c>b&&a>b){
+		printf("%d %d %d",b,a,c);
+	}
+	else if(c>a&&c>b&&b>a){
+		printf("%d %d %d",a,b,c);
+	}
+	else{
+		printf("Khong hop le");
+	}
+	return 0;
+}
